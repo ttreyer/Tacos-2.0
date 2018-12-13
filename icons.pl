872547c2f4b81c45840f000ff6ca2ea224fef19f
Thierry Treyer <zehkae@gmail.com>
2018-12-13 13:17:18 +0100

    Render icons offline

---
+++ icons.pl
@@ -0,0 +1,26 @@
+use v5.20;
+use Mojo::File 'path';
+use Mojo::Template;
+
+my %sauces_fill = ('Fromagère' => 'f8e9b5', Ketchup => 'c83427', Mayonnaise => 'fff0af', Cocktail => 'e29460', Blanche => 'fff', Barbecue => '3a1a13', 'Américaine' => 'e5843b', 'Biggy burger'=> 'e9d07e', Tartare => 'e3e1d5', Curry => 'f7c748', Andalouse => 'c86815', 'Algérienne' => 'c58843', Marocaine => '75200d', Harissa => 'a00', 'Samouraï' => 'f0b175', Poivre => 'a68352');
+my %garnish_fill = ('Frites' => 'f1ed35', 'Cheddar'=> 'fceb4e', 'Gruyère'=> 'fffbc1', 'Salade'=> '77ff49', 'Tomate'=> 'ff583a', 'Oignons'=> 'dfe8b0', 'Carottes'=> 'f2b715', 'Cornichons'=> '86e87f');
+my %garnish_stroke = ('Frites' => 'e9cd00', 'Cheddar'=> 'ffd23f', 'Gruyère'=> 'f2ec9f', 'Salade'=> '20d302', 'Tomate'=> 'c61e00', 'Oignons'=> 'f7f7e1', 'Carottes'=> 'e89a00', 'Cornichons'=> '0eba01');
+
+my $sauce_template = Mojo::Template->new(vars => 1);
+$sauce_template->parse(path('templates/sauceEnabled.svg.ep')->slurp);
+
+foreach my $sauce (keys %sauces_fill) {
+  my $fill = $sauces_fill{ $sauce };
+  my $sauce_icon = $sauce_template->process({ color => $fill });
+  path("public/sauceEnabled-$sauce.svg")->spurt($sauce_icon);
+}
+
+my $garnish_template = Mojo::Template->new(vars => 1);
+$garnish_template->parse(path('templates/garnishEnabled.svg.ep')->slurp);
+
+foreach my $garnish (keys %garnish_fill) {
+  my $fill = $garnish_fill{ $garnish };
+  my $stroke = $garnish_stroke{ $garnish };
+  my $garnish_icon = $garnish_template->process({ color1 => $fill, color2 => $stroke });
+  path("public/garnishEnabled-$garnish.svg")->spurt($garnish_icon);
+}
--- tacos.pl
+++ tacos.pl
@@ -9,9 +9,6 @@ helper sizes_max_meat => sub { my %max = (M => 1, L => 1, 'L Mixte' => 3, XL => 
 helper meats => sub { 'Viande hachée', 'Escalope de poulet', 'Cordon bleu', 'Merguez', 'Nuggets', 'Kebab', 'Soudjouk', 'Végétarien' };
 helper garnishes => sub { 'Frites', 'Cheddar', 'Gruyère', 'Salade', 'Tomate', 'Oignons', 'Carottes', 'Cornichons' };
 helper sauces => sub { 'Fromagère', 'Ketchup', 'Mayonnaise', 'Cocktail', 'Blanche', 'Barbecue', 'Américaine', 'Biggy burger', 'Tartare', 'Curry', 'Andalouse', 'Algérienne', 'Marocaine', 'Harissa', 'Samouraï', 'Poivre' };
-helper sauces_color => sub { my %color =('Fromagère' => 'f8e9b5', Ketchup => 'c83427', Mayonnaise => 'fff0af', Cocktail => 'e29460', Blanche => 'fff', Barbecue => '3a1a13', 'Américaine' => 'e5843b', 'Biggy burger'=> 'e9d07e', Tartare => 'e3e1d5', Curry => 'f7c748', Andalouse => 'c86815', 'Algérienne' => 'c58843', Marocaine => '75200d', Harissa => 'a00', 'Samouraï' => 'f0b175', Poivre => 'a68352'); $color{ $_[1] } };
-helper garnish_color_fill => sub { my %color =('Frites' => 'f1ed35', 'Cheddar'=> 'fceb4e', 'Gruyère'=> 'fffbc1', 'Salade'=> '77ff49', 'Tomate'=> 'ff583a', 'Oignons'=> 'dfe8b0', 'Carottes'=> 'f2b715', 'Cornichons'=> '86e87f'); $color{ $_[1] } };
-helper garnish_color_stroke => sub { my %color =('Frites' => 'e9cd00', 'Cheddar'=> 'ffd23f', 'Gruyère'=> 'f2ec9f', 'Salade'=> '20d302', 'Tomate'=> 'c61e00', 'Oignons'=> 'f7f7e1', 'Carottes'=> 'e89a00', 'Cornichons'=> '0eba01'); $color{ $_[1] } };
 helper price => sub { my %prices = shift->sizes_prices; $prices{ shift() } };
 
 helper sqlite => sub { state $sqlite = Mojo::SQLite->new('sqlite:tacos.db') };
@@ -65,13 +62,6 @@ get '/delete/:tacos_id' => sub {
 
 get '/whatsapp' => 'whatsapp';
 
-get '/inline/:img_file/<:color>.svg' => sub {
-  my $c = shift;
-  $c->render(template =>$c->stash('img_file'),format => 'svg');};
-get '/inline/:img_file/:color1/<:color2>.svg' => sub {
-  my $c = shift;
-  $c->render(template =>$c->stash('img_file'),format => 'svg');
-};
 app->defaults('message' => undef);
 app->start;
 
@@ -176,7 +166,7 @@ Merci et bonne journée.</textarea>
         %= check_box garnish => $garnish, id => $garnish
         %= label_for $garnish => begin
           <%= $garnish %>
-          <span class="decoButton" style='--sauceURL:url("inline/garnishEnabled/<%= garnish_color_fill($garnish) %>/<%= garnish_color_stroke($garnish) %>.svg")'></span>
+          <span class="decoButton" style='--sauceURL:url("garnishEnabled-<%= $garnish %>.svg")'></span>
         %= end
       </span>
     % }
@@ -190,7 +180,7 @@ Merci et bonne journée.</textarea>
         %= check_box sauce => $sauce, id => $sauce
         %= label_for $sauce => begin
           <%= $sauce %>
-          <span class="decoButton" style='--sauceURL:url("inline/sauceEnabled/<%= sauces_color($sauce) %>.svg")'></span>
+          <span class="decoButton" style='--sauceURL:url("sauceEnabled-<%= $sauce %>.svg")'></span>
         %= end
       </span>
     % }
